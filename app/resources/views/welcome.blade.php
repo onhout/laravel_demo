0332e6886f3786aa72c493791462c27af78d3583
Peter Liu <onhout@gmail.com>
2021-08-23 14:16:00 -0700

    finished

--- app/resources/views/welcome.blade.php
+++ app/resources/views/welcome.blade.php
@@ -1,9 +1,17 @@
-<x-guest-layout>
+<x-app-layout>
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex mx-auto">
-                <h1 class="text-5xl">Demo App</h1>
+                <p class="text-5xl my-6">Demo App</p>
+            </div>
+        </div>
+        <div class="h-16 flex ">
+            <div class="grid grid-flow-row auto-rows-max mx-auto">
+                <p class="text-xl py-3">Welcome to my demo app built for CalMHSA. </p>
+                <p class="text-xl py-3">This is an app to demonstrate a url shortener using Laravel. </p>
+                <p class="text-xl py-3">Please register or login with user@example.com / password to add a url
+                    shortener. </p>
             </div>
         </div>
     </div>
-</x-guest-layout>
+</x-app-layout>
